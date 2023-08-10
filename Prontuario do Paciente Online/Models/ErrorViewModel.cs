8b09425271cf600930a59b0ca81bf7c955572a41
Lucas Timoteo <78829600+lucarques@users.noreply.github.com>
2023-08-09 21:38:02 -0300

    Adicionar arquivos de projeto.

---
+++ Prontuario do Paciente Online/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prontuario_do_Paciente_Online.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
