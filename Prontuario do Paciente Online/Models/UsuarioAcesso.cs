ea8fc75fc77832fa8bb887eff1d2dbcde99900fc
Lucas Timoteo <78829600+lucarques@users.noreply.github.com>
2023-08-25 19:44:02 -0300

    Inseri validacao de campo em cadastro do paciente

--- Prontuario do Paciente Online/Models/UsuarioAcesso.cs
+++ Prontuario do Paciente Online/Models/UsuarioAcesso.cs
@@ -1,12 +1,16 @@
 ﻿using Prontuario_do_Paciente_Online.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace Prontuario_do_Paciente_Online.Models
 {
     public class UsuarioAcesso
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "O campo Usuario é obrigatório.")]
         public string Usuario { get; set; }
+        [Required(ErrorMessage = "O campo E-mail é obrigatório.")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "O campo Senha é obrigatório.")]
         public string SenhaAcesso { get; set; }
         public EnumTipoAcesso TipoAcesso { get; set; }
     }
--- Prontuario do Paciente Online/ViewModels/PacientesViewModel.cs
+++ Prontuario do Paciente Online/ViewModels/PacientesViewModel.cs
@@ -1,18 +1,27 @@
 ﻿using Prontuario_do_Paciente_Online.Models;
 using Prontuario_do_Paciente_Online.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace Prontuario_do_Paciente_Online.ViewModels
 {
     public class PacientesViewModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo Idade é obrigatório.")]
         public int Idade { get; set; }
+        [Required(ErrorMessage = "O campo CPF é obrigatório.")]
         public string Cpf { get; set; }
+        [Required(ErrorMessage = "O campo Estado é obrigatório.")]
         public string Estado { get; set; }
+        [Required(ErrorMessage = "O campo Cidade é obrigatório.")]
         public string Cidade { get; set; }
+        [Required(ErrorMessage = "O campo Endereco é obrigatório.")]
         public string Endereco { get; set; }
+        [Required(ErrorMessage = "O campo Numero é obrigatório.")]
         public int Numero { get; set; }
+        [Required(ErrorMessage = "O campo Motivo é obrigatório.")]
         public string MotivoInternacao { get; set; }
         public AcompanhanteViewModel Acompanhante { get; set; }
         public EnumStatus? StatusPaciente { get; set; }
