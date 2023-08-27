d137c15a7a13c7a6f0469df124fcc41ef0a952fd
Lucas Timoteo <78829600+lucarques@users.noreply.github.com>
2023-08-26 21:13:06 -0300

    Implementa tela de usuario acesso com melhorias nos grids de cadastro

--- Prontuario do Paciente Online/Models/UsuarioAcesso.cs
+++ Prontuario do Paciente Online/Models/UsuarioAcesso.cs
@@ -6,12 +6,22 @@ namespace Prontuario_do_Paciente_Online.Models
     public class UsuarioAcesso
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = "O campo Usuario é obrigatório.")]
         public string Usuario { get; set; }
-        [Required(ErrorMessage = "O campo E-mail é obrigatório.")]
         public string Email { get; set; }
-        [Required(ErrorMessage = "O campo Senha é obrigatório.")]
         public string SenhaAcesso { get; set; }
         public EnumTipoAcesso TipoAcesso { get; set; }
+
+        public UsuarioAcesso() 
+        {
+
+        }
+        public UsuarioAcesso(int id, string usuario, string email, string senhaAcesso, EnumTipoAcesso tipoAcesso)
+        {
+            Id = id;
+            Usuario = usuario;
+            Email = email;
+            SenhaAcesso = senhaAcesso;
+            TipoAcesso = tipoAcesso;
+        }
     }
 }
---
+++ Prontuario do Paciente Online/ViewModels/UsuarioAcessoViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Prontuario_do_Paciente_Online.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Prontuario_do_Paciente_Online.ViewModels
+{
+    public class UsuarioAcessoViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O campo Usuario é obrigatório.")]
+        public string Usuario { get; set; }
+        [Required(ErrorMessage = "O campo E-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O e-mail informado não é valido")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "O campo Senha é obrigatório.")]
+        public string SenhaAcesso { get; set; }
+        public EnumTipoAcesso TipoAcesso { get; set; }
+    }
+}
